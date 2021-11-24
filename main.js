e6cabee798dbf335e19a5601d3be374587e17b75
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-11-24 20:10:08 +0100

    Make modal pop on condition

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 const closeBtn = document.querySelector(".x-close");
 const container = document.querySelector(".payment-container");
-const launch = document.querySelector(".payment-launch");
 
 closeBtn.addEventListener("click", closeModal);
 container.addEventListener("click", closeModal2);
 
-// Launch DOM can be replaced by any action that makes payment successful
-launch.addEventListener("click", openModal);
-
 // Closes the modal when closeBtn is clicked
 function closeModal() {
-  console.log(document.querySelector("body"));
   if (container.classList.contains("rem-modal")) {
   } else {
     container.classList.add("rem-modal");
@@ -50,3 +45,46 @@ function openModal() {
   container.style.display = "flex";
   document.querySelector("body").style.overflow = "hidden";
 }
+
+/**
+ *
+ * @param {user img} img
+ * @param {message} msg
+ * @param {nim received by the user} value
+ * @param {length of time the success modal should appear} seconds
+ */
+function successModal(img, msg, value, seconds) {
+  // Triggers the succes modal
+  openModal();
+
+  const user_img = document.querySelector(".user-avatar");
+  const msg_input = document.querySelector(".payment-address");
+  const received_val = document.querySelector(".payment-value");
+
+  // condition to pop the success modal
+  // condition can be changed
+  if (img && msg && value) {
+    user_img.src = img;
+    msg_input.textContent = msg;
+    received_val.textContent = value;
+
+    if (container) {
+      setTimeout(() => {
+        container.classList.add("rem-modal");
+
+        setTimeout(() => {
+          container.style.display = "none";
+          document.querySelector("body").style.overflow = "visible";
+        }, 1000);
+      }, seconds * 1000);
+    }
+  }
+}
+
+// Calls the function to trigger the modal
+successModal(
+  "/pig.svg",
+  "This is a message from your viewer, nice stream - good work!!!",
+  1000,
+  5
+);
